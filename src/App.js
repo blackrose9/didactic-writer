4e0d5688eb191958a7fe72f4343379ed5cd1a71f
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-10 23:49:32 +0300

    import /Components/Links in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Posts from './Components/Posts';
 import About from './Components/About';
+import Links from './Components/Links';
 
 function App() {
   return (
