deb49e5fddb4d9875d57657cb7c61a0295a62b0a
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-10 23:47:27 +0300

    Add Links page to project

---
+++ src/Components/Links.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+export class Links extends Component {
+   render(){
+      return(
+         <div>
+            <h1>Check out other stuff I have done</h1>
+            <h3><a href="https://blackrose722.blogspot.com/">Blogger</a></h3>
+            <h3><a href="https://extraordinarymonk.wordpress.com/">Extraordinary Monk</a></h3>
+            <h3><a href="https://blackrose9.github.io/">Personal Website</a></h3>
+         </div>
+      )
+      
+   }
+}
+
+export default Links;
