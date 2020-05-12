45fa15b396f61730bd1f16b784ae3b90eea37426
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-12 23:52:59 +0300

    add allowance for where posts will be displayed add component css file connect css file to posts.js

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
-import axios from 'axios'
+import React, {Component} from 'react';
+import axios from 'axios';
+import './component.css';
 
 export class Posts extends Component {
    state = {
@@ -22,6 +23,11 @@ export class Posts extends Component {
          <div>
             <h1>Stuff I write</h1>
             <p>Here will be where a list of the stuff I have written. Called using Wordpress Headless CMS <br/> So exciting!</p>
+            <div className="postContainer">
+                  <h3>PostTitle</h3>
+                  <p>This is a small snippet of what the post is about, lol</p>
+                  <a href="#">Check this post out</a>
+            </div>
          </div>
       )
    }
