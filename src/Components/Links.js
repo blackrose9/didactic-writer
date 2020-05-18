ca194eaa6a674f0db684babad0f3730a38f1da5c
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-18 17:28:38 +0300

    Make links into cards

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -1,14 +1,58 @@
 import React, {Component} from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+ import blogger from '../img/blogger1.png'
+ import em from '../img/em.jpg'
+ import me from '../img/me.png'
 
 export class Links extends Component {
    render(){
       return(
-         <div>
+         <Container>
             <h1>Check out other stuff I have done</h1>
-            <h3><a href="https://blackrose722.blogspot.com/">Blogger</a></h3>
-            <h3><a href="https://extraordinarymonk.wordpress.com/">Extraordinary Monk</a></h3>
-            <h3><a href="https://blackrose9.github.io/">Personal Website</a></h3>
-         </div>
+            <Row>
+               <Col>
+                  <Card style={{ width: '20rem' }}>
+                     <Card.Img variant="top" src={blogger}/>
+                     <Card.Body>
+                        <Card.Title>Blogger</Card.Title>
+                        <Card.Text>
+                           This is my main blog where I write.
+                        </Card.Text>
+                        <Button variant="info" href="https://blackrose722.blogspot.com/">Visit Blog</Button>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col>
+                  <Card style={{ width: '20rem' }}>
+                     <Card.Img variant="top" src={em}/>
+                     <Card.Body>
+                        <Card.Title>Extraordinary Monk</Card.Title>
+                        <Card.Text>
+                           This is another blog where I write, I guess more meaningful stuff, I guess.
+                        </Card.Text>
+                        <Button variant="info" href="https://extraordinarymonk.wordpress.com/">Visit Blog</Button>
+                     </Card.Body>
+                  </Card>
+               </Col>
+               <Col>
+                  <Card style={{ width: '20rem' }}>
+                     <Card.Img variant="top" src={me}/>
+                     <Card.Body>
+                        <Card.Title>My Website</Card.Title>
+                        <Card.Text>
+                           Take a look at the other side of my life.
+                        </Card.Text>
+                        <Button variant="info" href="https://blackrose9.github.io/">Personal Website</Button>
+                     </Card.Body>
+                  </Card>
+               </Col>
+            </Row>
+         </Container>
       )
       
    }
