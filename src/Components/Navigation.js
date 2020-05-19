0304de00bee78496be7b1c14e7d2b8a0ddbaa983
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-19 23:12:06 +0300

    Add bootstrap styling to the navigation bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { NavLink } from "react-router-dom"
+import Nav from 'react-bootstrap/Nav'
 
 const Navigation = () => {
    return (
       <div>
-         <NavLink to='/'>Home</NavLink>
-         <NavLink to='/posts'>Articles</NavLink>
-         <NavLink to='/links'>My Links</NavLink>
+         <Nav variant='tabs' defaultActivityKey='/'>
+            <Nav.Item>
+               <Nav.Link><NavLink to='/'>Home</NavLink></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+               <Nav.Link><NavLink to='/posts'>Articles</NavLink></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+               <Nav.Link><NavLink to='/links'>My Links</NavLink></Nav.Link>
+            </Nav.Item>
+         </Nav>
       </div>
    )
 }
