a184d78147011d1168fcf708ae23a7c3b20664d4
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-23 23:14:36 +0300

    Updates

---
+++ precache-manifest.e127d0d76de1081b924ba5cb8a2080a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a93f5078693613470a576b228ff2ce2",
+    "url": "/didactic-writer/index.html"
+  },
+  {
+    "revision": "5be9ece43d3c836b108c",
+    "url": "/didactic-writer/static/css/2.9de105d4.chunk.css"
+  },
+  {
+    "revision": "a10c0a0fbd2fdc0e7b9d",
+    "url": "/didactic-writer/static/css/main.aef83898.chunk.css"
+  },
+  {
+    "revision": "5be9ece43d3c836b108c",
+    "url": "/didactic-writer/static/js/2.b5940119.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/didactic-writer/static/js/2.b5940119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10c0a0fbd2fdc0e7b9d",
+    "url": "/didactic-writer/static/js/main.aec0a129.chunk.js"
+  },
+  {
+    "revision": "9bd1eb30b05a9a858baf",
+    "url": "/didactic-writer/static/js/runtime-main.591a796d.js"
+  }
+]);
