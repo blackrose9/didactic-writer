db14554237d28e1cabe8089ee05e2a7fd17dbfac
blackrose9 <sarah.mwendwa@outlook.com>
2020-05-24 01:08:01 +0300

    Updates

---
+++ precache-manifest.33fb0b1945cb431ebaa66ed1b944734b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ddb152cc0f941efb2f40ee8428629a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5be9ece43d3c836b108c",
+    "url": "./static/css/2.9de105d4.chunk.css"
+  },
+  {
+    "revision": "a10c0a0fbd2fdc0e7b9d",
+    "url": "./static/css/main.aef83898.chunk.css"
+  },
+  {
+    "revision": "5be9ece43d3c836b108c",
+    "url": "./static/js/2.b5940119.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b5940119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10c0a0fbd2fdc0e7b9d",
+    "url": "./static/js/main.aec0a129.chunk.js"
+  },
+  {
+    "revision": "c051b7394a21b4db42d6",
+    "url": "./static/js/runtime-main.fd94d7ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/didactic-writer/precache-manifest.e127d0d76de1081b924ba5cb8a2080a4.js"
+  "./precache-manifest.33fb0b1945cb431ebaa66ed1b944734b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/didactic-writer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
